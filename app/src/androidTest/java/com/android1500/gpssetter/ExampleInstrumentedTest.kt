0f9482fe9708b7d999c2eb7181920a80cddf79c8
USMany <131568484+patubole@users.noreply.github.com>
2025-05-29 14:35:38 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/android1500/gpssetter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/android1500/gpssetter/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.android1500.gpssetter
+package com.android1500.gpssetter2
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.android1500.gpssetter", appContext.packageName)
+        assertEquals("com.android1500.gpssetter2", appContext.packageName)
     }
 }
